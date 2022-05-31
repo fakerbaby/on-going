bf07d8870d592c6a09d4687e37153f3adc6fcbe7
fakerbaby <794598816@qq.com>
2022-05-31 23:46:15 +0800

    add transformer

---
+++ transformers/examples/pytorch/text-classification/run.sh
@@ -0,0 +1,13 @@
+export TASK_NAME=mrpc
+
+
+export CUDA_VISIBLE_DEVICES=3
+
+accelerate launch run_glue_no_trainer.py \
+  --model_name_or_path bert-base-cased \
+  --task_name $TASK_NAME \
+  --max_length 128 \
+  --per_device_train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --output_dir /tmp/$TASK_NAME/conda
