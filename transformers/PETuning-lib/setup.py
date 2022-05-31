6d0596d2fb404e1e14606387ab91776542422781
fakerbaby <794598816@qq.com>
2022-06-01 01:51:17 +0800

    init

---
+++ transformers/PETuning-lib/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PETuninglib',
+      version='0.1.0',
+      description='Utilities',
+      author='wei shen',
+      author_email='',
+      url='',
+      packages=['loralib'],
+     )
